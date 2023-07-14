57924028f70cc29954da2947a2a3ed622ed2912b
jonas nasc <110119420+jonassnasc7@users.noreply.github.com>
2023-07-14 14:33:38 -0300

    Add files via upload

---
+++ node-main/ATIVIDADE CORE MODULES/FS/index.js
@@ -0,0 +1,21 @@
+    // ATIVIDADE 01
+const fs = require('fs')
+
+//     fs.readFile('teste.txt', 'utf8', (err, data) => {
+//         if (err) {
+//             console.log(err) 
+//         }
+//         console.log(data)
+//     })
+
+
+    // ATIVIDADE 02
+// const fs = require('fs')
+//     fs.open('teste2.txt', 'w', function (err) {
+//         if
+//         (err) throw(err)
+        
+//         console.log('jonaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas')
+//     })
+
+    // ATIVIDADE 03
